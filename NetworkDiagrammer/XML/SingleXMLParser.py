3eff3bb32d32ed532f9f1059fd7ea8888397e679
CyPH3RSkULL5 <noah.beckman@mymail.champlain.edu>
2022-10-27 12:59:40 -0400

    Major progress on parsing script

--- NetworkDiagrammer/XML/SingleXMLParser.py
+++ NetworkDiagrammer/XML/SingleXMLParser.py
@@ -1,19 +1,87 @@
 import xml.etree.ElementTree as ET
+import pandas as pd
+
+#-------
+
+raw_Ip_List = []
+ScannedIPList = []
+numOfScannedIps = 0
+
+#This function is responsible for copying the xml informatin to a data frame
+def host_intr_docs(xml_doc):
+	#Get all atributes in the xml file
+	attr = xml_doc.attrib
+
+	#for eac of the xml elements within the address field
+	for xml in xml_doc.iter('address'):
+
+		#Copy the element to the datafram
+		doc_dict = attr.copy()
+
+		#update the dataframe
+		doc_dict.update(xml.attrib)
+
+		#set the dataframe data to the text of the xml attribute
+		doc_dict['data'] = xml.text
+
+		yield doc_dict
+
+def port_intr_docs(xml_doc):
+
+	attr = xml_doc.attrib
+
+	for xml in xml_doc.iter('port'):
+
+		#Copy the element to the datafram
+		doc_dict = attr.copy()
+
+		#update the dataframe
+		doc_dict.update(xml.attrib)
+
+		#set the dataframe data to the text of the xml attribute
+		doc_dict['data'] = xml.text
+
+		yield doc_dict
+
+#This function takes in a list with duplicates, removes the duplicates, and sets the results to a new list
+def clearDuplicates(dupList, newList):
+	newList = [*set(dupList), 1]
 
 
-myTree = ET.parse('elrond_Scan_XML.xml')
+#This parses the xml file into a variable
+etree = ET.parse("Full_Scan_XML.xml")
+#This sets a dataframe content from the function above. 
+Host_df = pd.DataFrame(list(host_intr_docs(etree.getroot())))
 
-myRoot = myTree.getroot()
+Port_df = pd.DataFrame(list(port_intr_docs(etree.getroot())))
 
-##print(myRoot[0].attrib)
+#This loop can get the contents of a column. In this case a target
+for item in Host_df['addr']:
+	#print(item)
+	raw_Ip_List.append(item)
 
-#for x in myRoot[3]:
-#	print(x.tag, x.attrib)
+ScannedIPList = [*set(raw_Ip_List)]
+ScannedIPList.sort()
 
-##for x in myRoot[2]:
-##	print(x.text)
+print(ScannedIPList)
 
-for host in myRoot.findall('host'):
-	print(host.tag, host.attrib)
-	for port in host.findall('port'):
-		print (port.tag)
+numOfScannedIps = len(ScannedIPList)
+
+print('Number of Ips: ', numOfScannedIps)
+
+
+
+
+
+
+#for event, elem in ET.iterparse("elrond_Scan_XML.xml"):
+#	if elem.tag == 'address':
+#		
+#		printText = ET.tostring(elem, method="xml")
+#		print(printText)
+#
+#		#print(addressString.split(':'))
+#	
+#
+#	#if elem.tag == 'port':
+#		#print(ET.tostring(elem, 'UTF-8', 'xml'))
